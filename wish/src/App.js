2a75a5ed18c6b7ac8e6dd04d4fb2c934154918c6
Ayman-24 <ayman.it24@gmail.com>
2024-02-13 02:09:16 +0530

    hashrouter added

--- wish/src/App.js
+++ wish/src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import NameModal from "./NameModal";
+import { HashRouter, Route } from "react-router-dom";
 
 const phrases = [
   "NO(dont)",
@@ -18,7 +19,6 @@ function App() {
   const yesButtonSize = noCount * 20 + 16;
   const [isModalOpen, setIsModalOpen] = useState(true);
   const [userName, setUserName] = useState("");
-  // const [message, setMessage] = useState(""); // State to manage the message text
 
   function handleNoClick() {
     setNoCount(noCount + 1);
@@ -32,47 +32,55 @@ function App() {
     setUserName(name);
   };
 
+  // The main content of your app is now wrapped in a Route component for potential future routing
   return (
-    <div>
-      <div>
-        <h1 className="header">Welcome{userName ? `, ${userName}` : ""}!</h1>
-        <NameModal
-          isOpen={isModalOpen}
-          onRequestClose={() => setIsModalOpen(false)}
-          onSave={handleNameSave}
-        />
-      </div>
-      <div className="valentine-container">
-        {yesPressed ? (
-          <>
-            <img
-              alt="bear with hearts"
-              src="https://gifdb.com/images/high/success-celebration-djtwshw329q808jo.gif"
+    <HashRouter>
+      <Route path="/" exact>
+        <div>
+          <div>
+            <h1 className="header">
+              Welcome{userName ? `, ${userName}` : ""}!
+            </h1>
+            <NameModal
+              isOpen={isModalOpen}
+              onRequestClose={() => setIsModalOpen(false)}
+              onSave={handleNameSave}
             />
-            {/* Display the text input for the message under the GIF */}
-            <div>
-              <p>everyone deserves a valentine</p>
-            </div>
-          </>
-        ) : (
-          <>
-            <div className="question">Will you be my Valentine this year?</div>
-            <div>
-              <button
-                className="yesButton"
-                style={{ fontSize: `${yesButtonSize}px` }}
-                onClick={() => setYesPressed(true)}
-              >
-                YES :)
-              </button>
-              <button onClick={handleNoClick} className="noButton">
-                {getNoButtonText()}
-              </button>
-            </div>
-          </>
-        )}
-      </div>
-    </div>
+          </div>
+          <div className="valentine-container">
+            {yesPressed ? (
+              <>
+                <img
+                  alt="bear with hearts"
+                  src="https://gifdb.com/images/high/success-celebration-djtwshw329q808jo.gif"
+                />
+                <div>
+                  <p>everyone deserves a valentine</p>
+                </div>
+              </>
+            ) : (
+              <>
+                <div className="question">
+                  Will you be my Valentine this year?
+                </div>
+                <div>
+                  <button
+                    className="yesButton"
+                    style={{ fontSize: `${yesButtonSize}px` }}
+                    onClick={() => setYesPressed(true)}
+                  >
+                    YES :)
+                  </button>
+                  <button onClick={handleNoClick} className="noButton">
+                    {getNoButtonText()}
+                  </button>
+                </div>
+              </>
+            )}
+          </div>
+        </div>
+      </Route>
+    </HashRouter>
   );
 }
 
